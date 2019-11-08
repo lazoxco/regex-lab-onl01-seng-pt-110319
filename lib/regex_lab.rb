53173e5104505942e1d5ae16cc1ed74072241a61
Nory Lazo <nlazo@icloud.com>
2019-11-08 04:13:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,19 @@
-def starts_with_a_vowel?(word)
+# word = ["Excellent", "incredible", "Absolute", "unreal", "Inconceivable" "octopus", "crazy"]
 
+def starts_with_a_vowel?(word)
+  if word.match(/^[aeiouAEIOU]\w+/)
+    true 
+  else 
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\b[Uu]n[a-z]*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/.{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
